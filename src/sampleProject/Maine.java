d6b1117bfad0340e0cffdc7aaa9a493cd0a27bef
Russell <russell.skorina@gmail.com>
2017-05-09 10:27:18 -0600

    Maine Joke

--- src/sampleProject/Maine.java
+++ src/sampleProject/Maine.java
@@ -5,4 +5,11 @@ public class Maine {
 	public Maine(){
 		System.out.println("Maine or Main? Who Knows");
 	}
+	
+	public void bruteForceJokeMethod(){
+		for(int i = 0; i < 100; i++){
+			System.out.println("Why is " + i + "afraid of " + i+1 + "?");
+			System.out.println("Because " + i +  i+1 + i+2);
+		}
+	}
 }
